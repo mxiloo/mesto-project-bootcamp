e097a1eeb00506c530ba885e4b08704403ba0fa4
Maxim Kharin <kharinmaxim123@gmail.com>
2023-05-24 14:54:22 +0500

    js & html fixed

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+// Переменные
+const modalPost = document.getElementById('modal-post');
+const modalImage = document.getElementById('modal-image');
+const textareaName = document.getElementById('textarea-name');
+const textareaDescription = document.getElementById('textarea-description');
+const modalProfile = document.getElementById('modal-profile');
+const description = document.getElementById('description');
+const name = document.getElementById('name');
+const modal__image = document.querySelector('.modal__image');
+const modal__text = document.querySelector('.modal__text');
+const editBtn = document.getElementById('edit-btn');
+const closeBtn =document.getElementById('modal-close-btn');
+const addBtn =document.getElementById('add-btn');
+const closeBtnPost =document.getElementById('modal-close-btn-post');
+
+
 function addNewCard (name, link) {
     const galleryTemplate = document.querySelector('#card-template').content;
     const galleryList = document.querySelector('.gallery__wrapper');
 
 // дубликат узла
-    let cardElement  = galleryTemplate.querySelector('.gallery__item').cloneNode(true);
+    const cardElement  = galleryTemplate.querySelector('.gallery__item').cloneNode(true);
 
     cardElement.querySelector('.gallery__delete-button').addEventListener('click', deleteCard)
     cardElement.querySelector('.gallery__image-button').addEventListener('click', openImage)
@@ -13,7 +29,6 @@ function addNewCard (name, link) {
     cardElement.querySelector('.gallery__title').textContent = name;
 
     galleryList.prepend(cardElement);
-
 }
 
 const initialCards = [
@@ -48,21 +63,18 @@ for (let i = 0; i < initialCards.length; i++) {
 }
 
 // Добавление поста
-const addPost = document.getElementById('modal-save-btn-post')
-addPost.onclick = () => {
-    /*const createPost = galleryTemplate.querySelector('.gallery__item').cloneNode(true);*/
-
-    let createText = document.getElementById('textarea-name-post').value;
-    let createSrc = document.getElementById('textarea-src-post').value;
+document.getElementById('modal-post').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const createText = document.getElementById('textarea-name-post').value;
+    const createSrc = document.getElementById('textarea-src-post').value;
 
     addNewCard(createText,createSrc)
 
-    document.getElementById('modal-post').classList.remove('popup__open')
+    modalPost.classList.remove('popup__open')
 
     document.getElementById('textarea-name-post').value = '';
     document.getElementById('textarea-src-post').value = '';
-
-}
+})
 
 // Удаление карточки
 function deleteCard (e) {
@@ -72,66 +84,65 @@ function deleteCard (e) {
     deleteElement.remove()
 }
 
-// Открытие картинки поста
-function openImage (e) {
-    document.getElementById('modal-image').classList.add('popup__open')
-
-    const image = e.target.src;
-    const text = e.target.alt;
-
-    document.querySelector('.modal__image').src = image;
-    document.querySelector('.modal__text').textContent = text;
-    document.querySelector('.modal__text').alt = text;
-}
-
 // Кнопка лайка
 function like () {
     this.classList.toggle('gallery__like-button_active')
 }
 
-// Открытие Попап изменения профиля
-document.getElementById('edit-btn').addEventListener('click', function () {
-    document.getElementById('modal-profile').classList.add('popup__open')
-})
+// Открытие картинки поста
+function openImage (e) {
+    modalImage.classList.add('popup__open')
 
-// Закрытие Попап изменения профиля
-document.getElementById('modal-close-btn').addEventListener('click', function (ev) {
-    ev.preventDefault();
-    document.getElementById('modal-profile').classList.remove('popup__open')
-})
+    const image = e.target.src;
+    const text = e.target.alt;
+
+    modal__image.src = image;
+    modal__text.textContent = text;
+    modal__text.alt = text;
+}
+
+// Закрытие картинки поста
+document.getElementById('modal-close-btn-image').addEventListener('click', function () {
+    modalImage.classList.remove('popup__open')
+});
+
+// Открытие Попап
+openPopup = () => {
+    editBtn.addEventListener('click', function () {
+        modalProfile.classList.add('popup__open')
+    })
+    addBtn.addEventListener('click', function (){
+        modalPost.classList.add('popup__open')
+    })
+}
+
+// Закрытие Попап
+closePopup = () => {
+    closeBtn.addEventListener('click', function () {
+        modalProfile.classList.remove('popup__open')
+    })
+    closeBtnPost.addEventListener('click', function (){
+        modalPost.classList.remove('popup__open')
+    })
+}
+
+openPopup();
+closePopup();
 
 // Кнопка сохранения информации профиля
-document.querySelector('.modal__save-btn').addEventListener("click", function (ev){
+document.querySelector('.popup__profile').addEventListener("submit", function (ev){
     ev.preventDefault();
-    const name = document.getElementById('name');
-    name.textContent = document.getElementById('textarea-name').value;
 
+    name.textContent = textareaName.value;
 
-    const description = document.getElementById('description');
-    description.textContent = document.getElementById('textarea-description').value;
+    description.textContent = textareaDescription.value;
 
-    document.getElementById('modal-profile').classList.remove('popup__open');
+    modalProfile.classList.remove('popup__open');
 
 
     document.getElementById('textarea-name').value = "";
     document.getElementById('textarea-description').value = ""
 })
-
-// Открытие Попап добавления поста
-document.getElementById('add-btn').addEventListener('click', function (){
-    document.getElementById('modal-post').classList.add('popup__open')
-})
-
-// Закрытие Попап добавления поста
-document.getElementById('modal-close-btn-post').addEventListener('click', function (){
-    document.getElementById('modal-post').classList.remove('popup__open')
-})
-
-// Закрытие картинки поста
-document.getElementById('modal-close-btn-image').addEventListener('click', function () {
-    document.getElementById('modal-image').classList.remove('popup__open')
-});
-
 
 
 
